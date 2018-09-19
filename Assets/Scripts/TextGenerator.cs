332fb2030b2989977d13e6756dfb59df6764e1f5
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2018-09-19 01:36:10 -0400

    It's been awhile, I forgot what this was about

--- Assets/Scripts/TextGenerator.cs
+++ Assets/Scripts/TextGenerator.cs
@@ -5,22 +5,40 @@ using UnityEngine.UI;
 
 public class TextGenerator : MonoBehaviour
 {
-    
-    public Text title;
-    public Text date;
+    /*
+     *  Tasks:
+     *      1. Break down title into 2 parts. [1. Relevant meme 2. Art Flair]
+     *      2. Make dates a random date range.
+     */
+    public Text titleAndDate;
     public Text medium;
     public Text dimension;
 
-    public string[] titleBank;
+    public string[] titleStart;
+    public string[] titleEnd;
     public string[] dateBank;
     public string[] mediumBank;
     public string[] dimensionBank;
 
     public void Start()
     {
-
-
-
+        GenNewLabel();
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenNewLabel();
+            print("Gen new label");
+        }    
+    }
+
+    private void GenNewLabel()
+    {
+        string datePicker = dateBank[Random.Range(0, dateBank.Length)];
+        titleAndDate.text = "<b><i>" +titleStart[Random.Range(0,titleStart.Length)]+" "+titleEnd[Random.Range(0, titleEnd.Length)]+"</i></b>"+ ", "+datePicker;
+        medium.text = mediumBank[Random.Range(0, mediumBank.Length)];
+        dimension.text = dimensionBank[Random.Range(0, dimensionBank.Length)];
+    }
 }
